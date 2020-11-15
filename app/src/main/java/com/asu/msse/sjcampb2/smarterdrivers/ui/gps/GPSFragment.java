5d816397faeebe201322af7ea166411d499307f0
Stu Campbell <stucampbell.git@gmail.com>
2020-11-14 23:50:03 -0700

    working map

--- app/src/main/java/com/asu/msse/sjcampb2/smarterdrivers/ui/gps/GPSFragment.java
+++ app/src/main/java/com/asu/msse/sjcampb2/smarterdrivers/ui/gps/GPSFragment.java
@@ -1,34 +1,152 @@
 package com.asu.msse.sjcampb2.smarterdrivers.ui.gps;
 
+import com.asu.msse.sjcampb2.smarterdrivers.R;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.asu.msse.sjcampb2.smarterdrivers.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GPSFragment extends Fragment {
 
-    private GPSViewModel gpsViewModel;
+    MapView mMapView;
+    private GoogleMap googleMap;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        gpsViewModel =
-                new ViewModelProvider(this).get(GPSViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gps, container, false);
-        gpsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_gps, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @SuppressLint("MissingPermission")
             @Override
-            public void onChanged(@Nullable String s) {
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng asu0 = new LatLng(33.417919, -111.936266);
+                LatLng asu1 = new LatLng(33.427000, -111.956200);
+                LatLng asu2 = new LatLng(33.437500, -111.976300);
+                LatLng asu3 = new LatLng(33.388082, -111.965887);
+
+                googleMap.addMarker(new MarkerOptions()
+                        .position(asu0)
+                        .title("Vehicle ID: 0001")
+                        .snippet("Status:\n" +"\tOn Road")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                );
+                googleMap.addMarker(new MarkerOptions()
+                        .position(asu1).title("Vehicle ID: 0002")
+                        .snippet("Status:\n" + "\tExcessive Speed!\n" + "\tLow Tire Pressure!")
+                );
+                googleMap.addMarker(new MarkerOptions()
+                        .position(asu2).title("Vehicle ID: 0004")
+                        .snippet("Status:\n" + "\tOn Road")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                );
+                googleMap.addMarker(new MarkerOptions()
+                        .position(asu3).title("Vehicle ID: 0003")
+                        .snippet("Status:\n" + "\tOver Temp Warning!")
+                );
+
+                Context mContext = getContext();
+
+                googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                    @Override
+                    public View getInfoWindow(Marker arg0) {
+                        return null;
+                    }
+
+                    @Override
+                    public View getInfoContents(Marker marker) {
+
+                        LinearLayout info = new LinearLayout(mContext);
+                        info.setOrientation(LinearLayout.VERTICAL);
+
+                        TextView title = new TextView(mContext);
+                        title.setTextColor(Color.BLACK);
+                        title.setGravity(Gravity.CENTER);
+                        title.setTypeface(null, Typeface.BOLD);
+                        title.setText(marker.getTitle());
+
+                        TextView snippet = new TextView(mContext);
+                        snippet.setTextColor(Color.GRAY);
+                        snippet.setText(marker.getSnippet());
+
+                        info.addView(title);
+                        info.addView(snippet);
+
+                        return info;
+                    }
+
+                });
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(asu1).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
             }
         });
-        return root;
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
--- app/src/main/java/com/asu/msse/sjcampb2/smarterdrivers/ui/inventory/InventoryFragment.java
+++ app/src/main/java/com/asu/msse/sjcampb2/smarterdrivers/ui/inventory/InventoryFragment.java
@@ -1,20 +1,13 @@
 package com.asu.msse.sjcampb2.smarterdrivers.ui.inventory;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.TabHost;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
