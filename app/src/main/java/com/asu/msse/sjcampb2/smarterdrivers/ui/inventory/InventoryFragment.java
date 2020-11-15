326fa129b2e5c812210f15c820aecf5cb4f0efbc
Stu Campbell <stucampbell.git@gmail.com>
2020-11-14 18:14:47 -0700

    inc

--- app/src/main/java/com/asu/msse/sjcampb2/smarterdrivers/ui/inventory/InventoryFragment.java
+++ app/src/main/java/com/asu/msse/sjcampb2/smarterdrivers/ui/inventory/InventoryFragment.java
@@ -7,11 +7,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -33,6 +36,37 @@ public class InventoryFragment extends Fragment {
         inventoryViewModel =
                 new ViewModelProvider(this).get(InventoryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_inv, container, false);
+
+        
+
+        TabLayout tabl = root.findViewById(R.id.tabLayout);
+        tabl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.v("", "ON SELECTED FUCKING WORKS");
+                switch(tab.getPosition()) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         inventoryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
